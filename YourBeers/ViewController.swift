fe53d9aa3fd9133c2a900ed6c69c0c2aaf2cb455
T0705GST <t0705gst@prestadorcbmp.com.br>
2020-03-20 19:53:32 -0300

    Initial Commit

---
+++ YourBeers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YourBeers
+//
+//  Created by Gustavo Rodrigues Leitão on 20/03/20.
+//  Copyright © 2020 Gustavo Rodrigues Leitão. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
